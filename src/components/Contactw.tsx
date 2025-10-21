616a3146dc8d6d72f6e758d8784f9fbdd7fc6b28
morpex460 <53935895+morpex460@users.noreply.github.com>
2025-10-21 15:28:11 +0300

    Center

--- src/components/Contactw.tsx
+++ src/components/Contactw.tsx
@@ -47,7 +47,7 @@ const Contact = () => {
           </p>
         </div>
 
-        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-3 sm:gap-4 lg:gap-6 max-w-6xl mx-auto">
+         <div className="flex flex-wrap justify-center gap-4 sm:gap-6 lg:gap-8 max-w-5xl mx-auto"> 
           {contacts.map((contact, index) => (
             <a
               key={index}
